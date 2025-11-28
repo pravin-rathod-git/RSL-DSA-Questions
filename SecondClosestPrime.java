a5c655eab3650883e3484893a983fa80dfeca959
pravin-rathod-git <pr819270@gmail.com>
2025-11-28 18:14:43 +0530

    Second closest Prime Number

--- SecondClosestPrime.java
+++ SecondClosestPrime.java
@@ -1,3 +1,43 @@
 public class SecondClosestPrime {
-    
+    private static boolean isPrime(int n){
+        if(n<=1){
+            return false;
+        }
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+     static int SecClosest(int n){
+        int l=n-1;
+        int r=n+1;
+        int count=0;
+        if(isPrime(n)){
+            count++;
+        }
+        while(true){
+            if(l>=2 && isPrime(l)){
+                count++;
+                if(count==2){
+                    return l;
+                }
+            }
+             if(isPrime(r)){
+                count++;
+                if(count==2){
+                    return r;
+                }
+            }
+            l--;
+            r++;
+        }
+    }
+
+    public static void main(String[] args) {
+        int n=22;
+        System.out.println(SecClosest(n));
+    }
 }
