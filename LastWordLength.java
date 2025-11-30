9e3e9a3206b318332694cbf87baf480d4e4a8fde
pravin-rathod-git <pr819270@gmail.com>
2025-11-30 18:35:22 +0530

    Last word length

---
+++ LastWordLength.java
@@ -0,0 +1,31 @@
+public class LastWordLength {
+
+    public static int lengthOfLastWord(String s) {
+        int n = s.length();
+        int i = n - 1;
+
+        // Skip trailing spaces
+        while (i >= 0 && s.charAt(i) == ' ') {
+            i--;
+        }
+
+        int j = i;
+
+        // Count the last word
+        while (i >= 0 && s.charAt(i) != ' ') {
+            i--;
+        }
+
+        return j - i;
+    }
+
+    public static void main(String[] args) {
+        String s1 = "Hello World";
+        String s2 = "   fly me   to   the moon  ";
+        String s3 = "luffy is still joyboy";
+
+        System.out.println("Length of last word: " + lengthOfLastWord(s1)); // 5
+        System.out.println("Length of last word: " + lengthOfLastWord(s2)); // 4
+        System.out.println("Length of last word: " + lengthOfLastWord(s3)); // 6
+    }
+}
