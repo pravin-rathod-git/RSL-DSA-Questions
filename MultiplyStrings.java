9ec3dfa1ef1851c3589c08bfaaa0bacd72582765
pravin-rathod-git <pr819270@gmail.com>
2025-11-30 18:41:29 +0530

    Multiply String

---
+++ MultiplyStrings.java
@@ -0,0 +1,43 @@
+public class MultiplyStrings {
+
+    public static String multiply(String num1, String num2) {
+        if (num1.equals("0") || num2.equals("0")) return "0";
+
+        int n = num1.length();
+        int m = num2.length();
+        int[] result = new int[n + m];
+
+        // Multiply digits from right to left
+        for (int i = n - 1; i >= 0; i--) {
+            int digit1 = num1.charAt(i) - '0';
+            for (int j = m - 1; j >= 0; j--) {
+                int digit2 = num2.charAt(j) - '0';
+                int product = digit1 * digit2;
+                int sum = product + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+            }
+        }
+
+        // Convert array to string
+        StringBuilder sb = new StringBuilder();
+        for (int num : result) {
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        String num1 = "123";
+        String num2 = "456";
+
+        String product = multiply(num1, num2);
+        System.out.println(num1 + " * " + num2 + " = " + product); // 56088
+
+        System.out.println("Example 2: " + multiply("2", "3"));   // 6
+    }
+}
