d91f0cc23553e3219aef8d1e179cbbf00b6168cb
pravin-rathod-git <pr819270@gmail.com>
2025-11-30 22:00:09 +0530

    Rotate image by 90 degree

---
+++ RotateImage.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class RotateImage {
+    
+    public static void rotate(int[][] matrix) {
+        int n = matrix.length;
+
+        // Step 1: Transpose the matrix
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+
+        // Step 2: Reverse each row
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
+            }
+        }
+    }
+
+    // Utility function to print matrix
+    public static void printMatrix(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int val : row) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Main function
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Original Matrix:");
+        printMatrix(matrix);
+
+        rotate(matrix);
+
+        System.out.println("\nRotated Matrix (90 degrees clockwise):");
+        printMatrix(matrix);
+    }
+}
